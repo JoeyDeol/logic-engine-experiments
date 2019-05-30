a89975e4c01c4cef004d6aef69b31153ce1afad6
JoeyDeol <gurjothdeol@gmail.com>
2019-05-30 14:58:16 -0400

    Create Search plugin

---
+++ layers/logic/plugins/search.js
@@ -0,0 +1,10 @@
+const Search = (() => {
+  class Search {
+    constructor() {
+      this.type = 'data-source';
+      console.log('Search plugin has been instantiated');
+    }
+  }
+  
+  return Search;
+})();
