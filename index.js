11fc48c320bbc04c772c99abef02b813ffe5b1f8
JoeyDeol <gurjothdeol@gmail.com>
2019-06-04 15:49:48 -0400

    Remove unnecessary code from index.js

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
 const core = new Core([Search, Sayt, Events]);
-
-core.fetchData("search");
-core.fetchData("sayt");
-
-// const originalAdd = window.addEventListener;
-// window.addEventListener = (eventName, callback) => {
-//   originalAdd.call(window, eventName, (event) => {
-//     core.collectEvent(event);
-//     callback(event);
-//   });
-// };
-
-// window.addEventListener('click', () => console.log('click'));
