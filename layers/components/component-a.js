b20655a23cc563ca654041c7ae225f73b6e50970
JoeyDeol <gurjothdeol@gmail.com>
2019-06-05 17:34:22 -0400

    WIP for updating my component to be stateful

--- layers/components/component-a.js
+++ layers/components/component-a.js
@@ -1,16 +1,29 @@
-const componentATemplate = document.createElement('template');
-componentATemplate.innerHTML= `<h2>This is Component A!</h2>`;
-
 class ComponentA extends HTMLElement {
   constructor() {
     super();
+    this.state = {};
 
+
+    // Define Component Template
+    const componentATemplate = document.createElement('template');
+    componentATemplate.innerHTML = `
+      <h2>This is Component A!</h2>
+      <p>The current state of the component is ${this.state.value || 'placeholder'}</p>
+    `;
+
+    // Create Shadow DOM node
     this.attachShadow({ 'mode': 'open' });
     this.shadowRoot.appendChild(componentATemplate.content.cloneNode(true));
   }
 
   connectedCallback() {
+    this.state = {
+      ...this.state,
+      value: 'connectedCallBack',
+    }
+  }
 
+  render() {
   }
 }
 
