b91dcb18b80f885b4a53077babbc620c1850a8a0
JoeyDeol <gurjothdeol@gmail.com>
2019-06-06 16:52:22 -0400

    WIP for updating events plugin to process registered event callbacks

--- layers/logic/plugins/events.js
+++ layers/logic/plugins/events.js
@@ -11,9 +11,15 @@ const Events = (() => {
     }
 
     registerWantedEvents(eventName, callback) {
-      window.addEventListener(eventName, callback);
-      window.addEventListener(eventName, this.collect);
-      window.addEventListener(eventName, this.process);
+      window.addEventListener(eventName, (event) => {
+        this.collect(event);
+        this.process(event, callback);
+
+        // TODO: Make sure to return a function that handles the event itself.
+      })
+      // window.addEventListener(eventName, callback);
+      // window.addEventListener(eventName, this.collect);
+      // window.addEventListener(eventName, this.process);
     }
 
     /*
@@ -26,7 +32,9 @@ const Events = (() => {
       /*
         The purpose of the process method is to respond to registered events and allow for a point in which other portions of the application can interact with events if they choose to. Also the process method is meant to 'clean up' after the collect method and update the collectedEvents array.
        */
-    process(event) {
+    process(event, callback) {
+      console.log('logging out event #process', event);
+      console.log('logging out event callback #process', callback);
       let currentEvent = this.collectedEvents.filter((item) => {
         return item.type === event.type;
       });
@@ -37,6 +45,7 @@ const Events = (() => {
 
       this.collectedEvents = remainingEvents;
       
+      // TODO Remove switch case, because this method should act in a consistent way no matter what event is emitted.
       switch (event.type) {
         case 'fetchProducts':
             console.log('A FETCH PRODUCTS EVENT HAS BEEN PROCESSED!');
@@ -55,6 +64,7 @@ const Events = (() => {
       The purpose of the emitUpdate method is to signal to the greater application that that events plugin has now concluded processing a given event. This is meant to act as a signal for when any given event has reached the end of its journey through the events plugin.
     */
     emitUpdate(event) {
+      console.log(`A ${event.type}_UPDATED event has been dispatched!`);
       window.dispatchEvent(new Event(`${event.type}_UPDATED`));
     }
   }
