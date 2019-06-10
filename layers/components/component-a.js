56800a7d416c0fea5fa6060cb7c96328438af109
JoeyDeol <gurjothdeol@gmail.com>
2019-06-10 12:38:43 -0400

    Update component to respond to events

--- layers/components/component-a.js
+++ layers/components/component-a.js
@@ -1,29 +1,70 @@
+// Define Component Template
+const componentATemplate = document.createElement('template');
+componentATemplate.innerHTML = `
+  <div class="componentA-root">
+    <h2>This is Component A!</h2>
+    <p>The current state of the component is default</p>
+  </div>
+`;
+
 class ComponentA extends HTMLElement {
   constructor() {
     super();
     this.state = {};
 
-
-    // Define Component Template
-    const componentATemplate = document.createElement('template');
-    componentATemplate.innerHTML = `
-      <h2>This is Component A!</h2>
-      <p>The current state of the component is ${this.state.value || 'placeholder'}</p>
-    `;
-
     // Create Shadow DOM node
     this.attachShadow({ 'mode': 'open' });
     this.shadowRoot.appendChild(componentATemplate.content.cloneNode(true));
+    this.$componentA = this.shadowRoot.querySelector('.componentA-root');
+
+    // Bind Methods
+    this.handleButtonPress = this.handleButtonPress.bind(this);
+    this.handleEvent = this.handleEvent.bind(this);
   }
 
   connectedCallback() {
     this.state = {
       ...this.state,
-      value: 'connectedCallBack',
+      value: 'Component Mounted',
     }
+
+    this.$componentA.innerHTML = `
+      <h2>This is Component A!</h2>
+      <p>The current state of the component is ${this.state.value}</p>
+      <button type="button">Fetch Sayt Data</button>
+    `
+
+    this.$componentA.querySelector('button').addEventListener('click', this.handleButtonPress);
+    document.addEventListener('fetchSaytProducts_UPDATED', this.handleEvent);
   }
 
-  render() {
+  handleButtonPress() {
+    window.dispatchEvent(new Event('fetchSaytProducts'));
+  }
+
+  handleEvent(event) {
+    const eventDetails = event.detail;
+    const productTemplates = []
+    
+    if (eventDetails) {
+      eventDetails.data.forEach((item) => {
+        const productTemplate = document.createElement('template');
+        productTemplate.innerHTML = `
+          <div>
+            <h3>${item.productName}</h3>
+            <p>${item.price}</p>
+            <p>${item.availability}</p>
+            <p>${item.rating}</p>
+          </div>
+        `;
+
+        productTemplates.push(productTemplate);
+      });
+    }
+
+    productTemplates.forEach((item) => {
+      this.$componentA.appendChild(item.content.cloneNode(true)); 
+    })
   }
 }
 
