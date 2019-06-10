2461e999b6c0a13af70f92e14abbc60305fcef66
JoeyDeol <gurjothdeol@gmail.com>
2019-06-10 10:16:37 -0400

    Create dummy data for sayt plugin to return after fetches

--- layers/logic/plugins/sayt.js
+++ layers/logic/plugins/sayt.js
@@ -1,3 +1,24 @@
+const saytData = [
+  {
+    productName: 'chocolate bar',
+    price: '1.99',
+    availability: true,
+    rating: 4,
+  },
+  {
+    productName: 'nintendo switch',
+    price: '499',
+    availability: true,
+    rating: 5,
+  },
+  {
+    productName: 'macbook pro',
+    price: '2399',
+    availability: true,
+    rating: 3,
+  },
+]
+
 const Sayt = (() => {
   class Sayt {
     constructor() {
@@ -8,8 +29,10 @@ const Sayt = (() => {
       };
     }
 
+    // The fetch method should return an object or an array with data-source plugin related data.
     fetch() {
-      console.log('THIS IS A METHOD THAT FETCHES SAYT DATA!');
+      console.log('THIS IS A METHOD THAT FETCHES SAYT DATA!', saytData);
+      return saytData
     }
   }
 
